33424e6bedc358ffe606dc1b0ee12da7260138bd
Debora Pacheco <dpacheco@inovvati.com.br>
2026-06-08 21:32:54 -0400

    refactor: streamline user creation logic in PrismaUserRepository

--- src/infra/database/repositories/PrismaUserRepository.ts
+++ src/infra/database/repositories/PrismaUserRepository.ts
@@ -1,17 +1,13 @@
 import { User } from "@domain/entities/User";
 import { IUserRepository } from "@domain/repositories/IUserRepository";
-import { PrismaClient } from "@prisma/client/extension";
 import { UserMapper } from "../mappers/UserMapper";
+import { PrismaClient } from "@prisma/client";
 
 export class PrismaUserRepository implements IUserRepository {
   constructor(private readonly prisma: PrismaClient) {}
   async create(user: User): Promise<User> {
     const created = await this.prisma.user.create({
-      data: {
-        name: user.name,
-        email: user.email.value,
-        passwordHash: user.passwordHash,
-      },
+      data: UserMapper.toPrismaCreate(user),
     });
     return UserMapper.toDomain(created);
   }
