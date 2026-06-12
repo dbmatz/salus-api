3cb1ca464ae5380251fd02ce7b97a8fd5d3bb220
Debora Pacheco <dpacheco@inovvati.com.br>
2026-06-11 20:42:06 -0400

    refactor: remove PostgreSQL adapter from PrismaClient initialization

--- src/infra/database/prisma/client.ts
+++ src/infra/database/prisma/client.ts
@@ -1,7 +1,3 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../../env";
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
